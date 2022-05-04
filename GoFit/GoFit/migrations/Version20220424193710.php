8be02a699f97971ff623f8c184f2a36f46f1e4cf
NourMakdouli <nour.makdouli@esprit.tn>
2022-05-04 18:31:31 +0100

    Mail + stat commande
    ajout du mail pour l'admin + ajout de l'attribut etat comm

---
+++ GoFit/GoFit/migrations/Version20220424193710.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220424193710 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE commande ADD statut TINYINT(1) DEFAULT NULL');
+        $this->addSql('ALTER TABLE produit CHANGE Path_Image Path_Image VARCHAR(150) DEFAULT \'NULL\'');
+        $this->addSql('ALTER TABLE reclamation CHANGE Reclamation_Id Reclamation_Id INT AUTO_INCREMENT NOT NULL, CHANGE Fk_utilisateur Fk_utilisateur INT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE commande DROP statut');
+        $this->addSql('ALTER TABLE produit CHANGE Path_Image Path_Image VARCHAR(150) NOT NULL');
+        $this->addSql('ALTER TABLE reclamation CHANGE Reclamation_Id Reclamation_Id INT NOT NULL, CHANGE Fk_utilisateur Fk_utilisateur INT NOT NULL');
+    }
+}
